c931353b3c40b24566e5747bbc68e258f19c79a7
Ryan Moore <ryanm.inbox@gmail.com>
2022-12-24 14:03:53 -0500

    add dashboard component on same page

--- front/src/App.js
+++ front/src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import Dashboard from './dashboard';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to <a href="https://www.github.com/yrom1/"><code>yrom1</code></a>'s site
         </p>
         <a
           className="App-link"
@@ -18,7 +20,8 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+      <Dashboard />
+    </div >
   );
 }
 
