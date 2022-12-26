1d1cd12791c80cbb959009f94e4530c6c9632263
Ryan Moore <ryanm.inbox@gmail.com>
2022-12-26 17:34:25 -0500

    remote hr on footer

--- front/src/components/Footer.js
+++ front/src/components/Footer.js
@@ -3,7 +3,6 @@ import React from 'react';
 function Footer() {
     return (
         <footer>
-            <hr></hr>
             <a href="https://www.github.com/yrom1/">GitHub</a>
         </footer>
     );
