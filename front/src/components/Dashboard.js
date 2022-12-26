1e2a742173f92381a0f2b9eb0f10e24523766a04
Ryan Moore <ryanm.inbox@gmail.com>
2022-12-26 13:20:42 -0500

    styling of home put quote in dashboard ignore ds_store

--- front/src/components/Dashboard.js
+++ front/src/components/Dashboard.js
@@ -5,7 +5,23 @@ function Dashboard() {
         document.title = "Ryan | Dashboard";
     }, []);
 
-    return <p>Dashboard</p>;
+    const [quote, setData] = useState(null);
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch('http://localhost:8000/quote');
+            const text = await response.text();
+            setData(text);
+        }
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+            {quote && <p>{quote}</p>}
+
+            <p>Dashboard</p>
+        </div>
+    );
 }
 
 export default Dashboard;
--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -1,26 +1,19 @@
 import React, { useState, useEffect } from 'react';
+import './Home.css';
 
 function Home() {
-    const [data, setData] = useState(null);
 
     useEffect(() => {
         document.title = "Ryan | Home";
-    }, []);
-
-    useEffect(() => {
-        async function fetchData() {
-            const response = await fetch('http://localhost:8000/quote');
-            const text = await response.text();
-            setData(text);
-        }
-        fetchData();
     }, []);
 
     return (
         <div>
             <p>Hi!</p>
             <p>My name is Ryan. I live in Toronto.</p>
-            {data && <p>{data}</p>}
+            <div>
+                <img src="/love-park.jpg" alt="Love Park" className="centered-image" />
+            </div>
         </div>
     );
 }
