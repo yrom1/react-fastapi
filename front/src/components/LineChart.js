db6e1c840cdb355ca1268632a6c191feb7f71813
Ryan Moore <ryanm.inbox@gmail.com>
2022-12-27 00:14:14 -0500

    use Loading component until its ready

--- front/src/components/LineChart.js
+++ front/src/components/LineChart.js
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import Plotly from 'plotly.js-basic-dist';
 import createPlotlyComponent from 'react-plotly.js/factory';
+import Loading from './Loading';
 
 const Plot = createPlotlyComponent(Plotly);
 
 function LineChart({ plotName }) {
     const [plotData, setPlotData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         async function fetchData() {
+            setIsLoading(true);
             const response = await fetch(`http://localhost:8000/plots/${plotName}`);
             const json = await response.json();
             setPlotData([{
@@ -17,9 +20,14 @@ function LineChart({ plotName }) {
                 type: 'scatter',
                 mode: 'lines'
             }]);
+            setIsLoading(false);
         }
         fetchData();
     }, [plotName]);
+
+    if (isLoading) {
+        return <Loading />;
+    }
 
     return (
         <Plot
