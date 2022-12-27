3934a377e41bf8373b946908386aa2935bbb430a
Ryan Moore <ryanm.inbox@gmail.com>
2022-12-26 23:51:25 -0500

    we have a plot!

--- front/src/components/BarChart.js
+++ front/src/components/BarChart.js
@@ -5,20 +5,24 @@ import createPlotlyComponent from 'react-plotly.js/factory';
 const Plot = createPlotlyComponent(Plotly);
 
 function BarChart({ plotName }) {
-    const [data, setData] = useState([]);
+    const [plotData, setPlotData] = useState([]);
 
     useEffect(() => {
         async function fetchData() {
-            const response = await fetch(`http://localhost:8000/plot/${plotName}`);
+            const response = await fetch(`http://localhost:8000/plots/${plotName}`);
             const json = await response.json();
-            setData(json.data);
+            setPlotData([{
+                x: json.x,
+                y: json.y,
+                type: 'bar'
+            }]);
         }
         fetchData();
     }, [plotName]);
 
     return (
         <Plot
-            data={data}
+            data={plotData}
             layout={{ title: 'Bar Chart' }}
             config={{ displayModeBar: false }}
             style={{ width: '100%', height: '100%' }}
