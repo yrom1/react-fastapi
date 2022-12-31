cae1916d597c7c75e8ed03d4dac011a3356812be
Ryan Moore <ryanm.inbox@gmail.com>
2022-12-31 03:15:38 -0500

    light dark themes except plots

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 React dashboard site with a fastapi backend!
 
 # notes
-I used `npx create-react-app front` to create the react app. Because React Router was such a pain and breaks the API so often I'm doing everything in pure React following `https://ncoughlin.com/posts/react-navigation-without-react-router/`.
+I used `npx create-react-app front` to create the react app. Because React Router was such a pain and breaks the API so often I'm doing everything in pure React following `https://ncoughlin.com/posts/react-navigation-without-react-router/`. Theme stuff from https://www.makeuseof.com/how-to-add-dark-mode-to-a-react-application/.
--- front/src/App.js
+++ front/src/App.js
@@ -5,10 +5,28 @@ import Dashboard from "./components/Dashboard"
 import Projects from "./components/Projects"
 import Footer from "./components/Footer"
 
+import './App.css';
+
 const App = () => {
   const [page, setPage] = useState(window.location.pathname);
   const [currentComponent, setCurrentComponent] = useState(null);
 
+  // const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState(
+    localStorage.getItem('theme') || 'light'
+  );
+  useEffect(() => {
+    localStorage.setItem('theme', theme);
+    document.body.className = theme;
+  }, [theme]);
+
+  const toggleTheme = () => {
+    if (theme === 'light') {
+      setTheme('dark');
+    } else {
+      setTheme('light');
+    }
+  };
   const handleLinkClick = (e, path) => {
     e.preventDefault();
     window.history.pushState({}, '', path);
@@ -16,7 +34,6 @@ const App = () => {
   }
 
   useEffect(() => {
-    console.log(2)
     switch (window.location.pathname) {
       case '/':
         setCurrentComponent(<Home />);
@@ -33,11 +50,12 @@ const App = () => {
   }, [window.location.pathname]);
 
   return (
-    <div className="App">
+    <div className={`App ${theme}`}>
       <header>
         <h3><a href="/" onClick={e => handleLinkClick(e, '/')}>Home</a></h3>
         <h3><a href="/dashboard" onClick={e => handleLinkClick(e, '/dashboard')}>Dashboard</a></h3>
         <h3><a href="/projects" onClick={e => handleLinkClick(e, '/projects')}>Projects</a></h3>
+        <button onClick={toggleTheme}>Toggle Theme</button>
       </header>
       {currentComponent}
       <Footer />
