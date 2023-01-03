f8c00335dd11f54c2b37b82e2189a0ff3ed73016
Ryan Moore <ryanm.inbox@gmail.com>
2023-01-02 23:41:52 -0500

    working light dark theme switcher (without page refresh!)

--- front/src/App.js
+++ front/src/App.js
@@ -18,29 +18,87 @@ const App = () => {
   );
 
   useEffect(() => {
-    localStorage.setItem('theme', theme);
+    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    const handleThemeChange = (e) => {
+      setTheme(e.matches ? 'dark' : 'light');
+    };
+
+    mediaQuery.addListener(handleThemeChange);
+    localStorage.setItem('theme', theme); // not needed, except maybe to talk to plotly
     document.body.className = theme;
+    return () => mediaQuery.removeListener(handleThemeChange);
   }, [theme]);
 
-  const toggleTheme = () => {
-    if (theme === 'light') {
-      setTheme('dark');
-    } else {
-      setTheme('light');
-    }
-  };
+
+  // const toggleTheme = () => {
+  //   if (theme === 'light') {
+  //     setTheme('dark');
+  //   } else {
+  //     setTheme('light');
+  //   }
+  // };
 
   return (
     <div className={`App ${theme}`}>
       {/* TODO what's an attractive way to arrange these? */}
-      <Dashboard />
+      <div>
+        <h1>theme</h1>
+        <body>The operating system's theme is: {theme}.</body>
+      </div>
+      {/* <Dashboard />
       <Projects />
       <p>–</p>
       <Quote />
       <p>–</p>
-      <Footer />
+      <Footer /> */}
     </div>
   );
 };
 
 export default App;
+
+// import React, { useEffect, useState } from 'react';
+
+// import Dashboard from "./components/Dashboard"
+// import Projects from "./components/Projects"
+// import Footer from "./components/Footer"
+// import Quote from "./components/Quote"
+// import Theme from "./components/Theme"
+
+// import './App.css';
+
+// const components = [Dashboard, Projects, Footer, Quote, Theme];
+
+// const App = () => {
+//   return (
+//     <div>
+//       <>
+//         {components.map(Component => (
+//           <>
+//             <hr />
+//             <Component />
+//             <hr />
+//           </>
+//         ))}
+//       </>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// import React, { useEffect, useState } from 'react';
+
+// function Theme() {
+//   const [theme, setTheme] = useState('light');
+
+
+//   return (
+    // <div>
+    //   <h1>theme</h1>
+    //   <body>The operating system's theme is: {theme}.</body>
+    // </div>
+//   );
+// }
+
+// export default Theme;
