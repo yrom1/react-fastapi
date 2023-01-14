5b7718182a2185f2ef46118189dd7bc82d261e76
Ryan Moore <ryanm.inbox@gmail.com>
2023-01-14 04:47:17 -0500

    think i found the problem

--- back/main.py
+++ back/main.py
@@ -17,10 +17,7 @@ _DEBUG = False
 
 app = FastAPI()
 
-# mostly copying bits from
-# https://testdriven.io/blog/fastapi-react/
-# Im guessing this is 3000 because it's React's default ¯\_(ツ)_/¯
-origins = ["http://localhost:3000", "localhost:3000"]
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
