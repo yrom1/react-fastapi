2be894298ed889eef3ae42af5e37c14becb4c45e
Ryan Moore <ryanm.inbox@gmail.com>
2023-01-21 16:20:04 -0500

    center dualcomponents

--- front/src/components/DualComponent.js
+++ front/src/components/DualComponent.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const DualComponent = ({ component1, component2 }) => {
     return (
-        <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
             <div style={{ flex: 1, borderRight: '1px solid black' }}>
                 {component1}
             </div>
