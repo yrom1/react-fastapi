ccac5220015915160ddfd12f8f1241ccc928ee68
Ryan Moore <ryanm.inbox@gmail.com>
2023-01-31 17:55:29 -0500

    center Expert component

--- front/src/components/Expert.js
+++ front/src/components/Expert.js
@@ -10,19 +10,15 @@ function Hi() {
     }, [])
 
     return (
-        <div>
-            <body>
-
-                <blockquote class="twitter-tweet">
-                    <p lang="en" dir="ltr">How to become expert at thing:<br></br>1 iteratively take on concrete projects and accomplish
-                        them depth wise, learning “on demand” (ie don’t learn bottom up breadth wise)<br></br>2 teach/summarize
-                        everything you learn in your own words<br></br>3 only compare yourself to younger you, never to others</p>&mdash;
-                    Andrej Karpathy (@karpathy) <a
-                        href="https://twitter.com/karpathy/status/1325154823856033793?ref_src=twsrc%5Etfw">November 7, 2020</a>
-                </blockquote>
-
-            </body>
-        </div >
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <blockquote class="twitter-tweet">
+                <p lang="en" dir="ltr">How to become expert at thing:<br></br>1 iteratively take on concrete projects and accomplish
+                    them depth wise, learning “on demand” (ie don’t learn bottom up breadth wise)<br></br>2 teach/summarize
+                    everything you learn in your own words<br></br>3 only compare yourself to younger you, never to others</p>&mdash;
+                Andrej Karpathy (@karpathy) <a
+                    href="https://twitter.com/karpathy/status/1325154823856033793?ref_src=twsrc%5Etfw">November 7, 2020</a>
+            </blockquote>
+        </div>
     );
 }
 
