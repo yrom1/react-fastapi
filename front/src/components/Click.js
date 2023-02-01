de8b218f3f830cf59b567079e12687a574def121
Ryan Moore <ryanm.inbox@gmail.com>
2023-01-31 22:22:04 -0500

    add swap between local and dev url in click

--- front/src/components/Click.js
+++ front/src/components/Click.js
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from "react";
 
 const Click = () => {
     const [clicks, setClicks] = useState(0);
+    const endpoint = (process.env.NODE_ENV == 'production' ? 'http://rymo.xyz' : 'http://localhost') + ':8000/clicks'
 
     useEffect(() => {
         const fetchData = async () => {
-            const response = await fetch("http://localhost:8000/clicks");
+            const response = await fetch(endpoint);
             const data = await response.json();
             setClicks(data.clicks);
         };
@@ -13,12 +14,12 @@ const Click = () => {
     }, []);
 
     const handleClick = async () => {
-        await fetch("http://localhost:8000/clicks", {
+        await fetch(endpoint, {
             method: "PUT"
         });
         // The number of clicks is incremented on the backend, so we just need to refetch the data
         const fetchData = async () => {
-            const response = await fetch("http://localhost:8000/clicks");
+            const response = await fetch(endpoint);
             const data = await response.json();
             setClicks(data.clicks);
         };
